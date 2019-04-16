7ce0f526c643213554d3fca762388226f3a13a7c
Maghiar Catalina <cata3379@yahoo.com>
2019-04-16 17:41:12 +0300

    created entities and added dependencies

---
+++ src/main/java/entity/Meeting.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public class Meeting {
+
+    public int id;
+    public String title;
+    public String location;
+    public LocalDateTime time;
+
+}
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class User {
+    public int id;
+    public String firstName;
+    public String lastName;
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
